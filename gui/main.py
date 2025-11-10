fde544ab487555a9d3b76a069c8d2b30a82a3b3d
Hayam <hayam@limitless-tech.ai>
2025-11-10 09:44:26 +0200

    logging in, working fine, tested

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 import json
 import re
 import socket
@@ -7,13 +8,25 @@ from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QWidget, QStackedWidget,
     QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QTabWidget, QFormLayout, QLineEdit, QMessageBox
 )
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
+
+"""IMPORTANT!!!!!!!!!
+Your jsonl_request() opens a new socket per request. That’s fine for REGISTER/LOGIN, but because your server uses connection-bound login (no tokens), anything like PROFILE.* must be sent over the same TCP connection you logged in on. When you implement Profile editing, create a small persistent session object and reuse it for those requests."""
+
+
+#for errors to show instead of crashing window
+def excepthook(exc_type, exc, tb):
+    traceback.print_exception(exc_type, exc, tb)
+    QMessageBox.critical(None, "Unhandled Error", f"{exc_type.__name__}: {exc}")
+sys.excepthook = excepthook
 
 #configuring client - will work only if server is running on localhost
 HOST = "127.0.0.1"
 PORT = 6000
 SOCKET_TIMEOUT = 4.0 #will only wait 4 seconds for a response from server
 ENCODING = "utf-8"
+
+
 
 def title_page(text):
     """Utility function to create a centered title page."""
@@ -190,7 +203,133 @@ class RegisterForm(QWidget):
         
         else:
             self.show_error(f"Unexpected response from server. {rtype}")
+
+class LoginForm(QWidget):
+    """validate fields then send AUTH.LOGIN_REQ to server"""
+    
+    logged_in = pyqtSignal(dict)
+
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
         
+        self.in_username = QLineEdit()
+        self.in_password = QLineEdit()
+        self.in_password.setEchoMode(QLineEdit.Password)
+        
+
+        for w in (self.in_username, self.in_password):
+            w.setMinimumWidth(250)
+            w.setMaximumWidth(400)
+
+        form = QFormLayout()
+        form.setLabelAlignment(Qt.AlignRight)
+        form.setFormAlignment(Qt.AlignHCenter | Qt.AlignCenter)
+        form.setContentsMargins(0,20,0,0)
+        form.setHorizontalSpacing(15)
+        form.setVerticalSpacing(10)
+
+          
+        form.addRow("Username:", self.in_username)
+        form.addRow("Password:", self.in_password)
+        
+
+        self.err = QLabel("")
+        self.err.setWordWrap(True)
+        self.err.setStyleSheet("color: red;")
+        self.err.setVisible(False)
+
+        self.btn_login = QPushButton("Log into account")
+        self.btn_login.setMinimumWidth(120)
+        self.btn_login.clicked.connect(self.on_submit)
+
+        card = QWidget()
+        card.setObjectName("login_card")
+        card.setStyleSheet("""
+            QWidget#login_card {
+                background: #ffffff;
+                border: 1px solid #e5e5e5;
+                border-radius: 12px;
+            }
+            QLineEdit {
+                padding: 8px 10px;
+                border: 1px solid #d0d0d0;
+                border-radius: 6px;
+            }
+            QLineEdit:focus {
+                border: 1px solid #7aa7ff;
+                outline: none;
+            }
+            
+        """)
+        card_layout = QVBoxLayout(card)
+        card_layout.setContentsMargins(30, 20, 30, 20)
+        card_layout.setSpacing(15)
+        card_layout.addLayout(form)
+        card_layout.addWidget(self.err)
+        card_layout.addSpacing(8)
+        card_layout.addWidget(self.btn_login,0, Qt.AlignHCenter)
+
+        root = QVBoxLayout(self)
+        root.setContentsMargins(0,0,0,0)
+        root.addStretch(1)
+        root.addWidget(card, 0, Qt.AlignHCenter)
+        root.addStretch(1)
+    
+    def show_error(self, message):
+        self.err.setText(message)
+        self.err.setVisible(True)
+    
+    def clear_error(self):
+        self.err.setText("")
+        self.err.setVisible(False)
+    
+    def validate(self):
+        username = self.in_username.text().strip()
+        password = self.in_password.text().strip()
+        
+        if not username or not USERNAME_RE.match(username):
+            return False, "Username is required."
+        if not password or not PASSWORD_RE.match(password):
+            return False, "Password is required."
+        
+        return True, {
+            "username": username,
+            "password": password,
+        }
+    
+    def on_submit(self):
+        self.clear_error()
+        ok, payload_or_msg = self.validate()
+        if not ok:
+            self.show_error(payload_or_msg)
+            return
+        req = {
+            "type": "AUTH.LOGIN_REQ",
+            "id": str(uuid.uuid4()),
+            "payload": payload_or_msg
+        }
+        try:
+            resp = jsonl_request(HOST, PORT, req)
+        except Exception as e:
+            self.show_error(f"Network error: {e}")
+            return
+        rtype = resp.get("type")
+        payload = resp.get("payload", {})
+        if rtype == "AUTH.LOGIN_RES":
+            user_preview = payload.get("user", {})
+            self.logged_in.emit(user_preview)
+            
+            self.in_username.clear()
+            self.in_password.clear()
+
+        elif rtype == "ERROR":
+            msg = payload.get("message", "Unknown error")
+            self.show_error(f"Error: {msg}")
+        
+        else:
+            self.show_error(f"Unexpected response from server. {rtype}")
+
 class RidePage(QWidget):
     """Ride page with sub-navigation: Overview | Chat | Ratings"""
     def __init__(self):
@@ -247,14 +386,16 @@ class MainWindow(QMainWindow):
         v_auth = QVBoxLayout(auth_page)
 
         tabs = QTabWidget()
-        tabs.addTab(title_page("Login Here"), "Login")
-        tabs.addTab(RegisterForm(), "Register")
+        self.login_tab = LoginForm()
+        self.register_tab = RegisterForm()
+        tabs.addTab(self.login_tab, "Login")
+        tabs.addTab(self.register_tab, "Register")
 
-        continue_btn = QPushButton("Continue")
-        continue_btn.clicked.connect(self.goto_app)
+        # continue_btn = QPushButton("Continue")
+        # continue_btn.clicked.connect(self.goto_app)
 
         v_auth.addWidget(tabs, 1)
-        v_auth.addWidget(continue_btn)
+        # v_auth.addWidget(continue_btn)
         self.root.addWidget(auth_page)
 
         # ---- App Page ----
@@ -278,7 +419,12 @@ class MainWindow(QMainWindow):
 
         # Right stack (Profile / Schedule / Ride)
         self.stack = QStackedWidget()
-        self.stack.addWidget(title_page("Your Profile"))
+        self.profile_label = QLabel("Profile Information")
+        profile_page = QWidget()
+        pv = QVBoxLayout(profile_page)
+        pv.addWidget(self.profile_label, 0 , Qt.AlignCenter)
+        pv.addStretch(1)
+        self.stack.addWidget(profile_page)
         self.stack.addWidget(title_page("Your Schedule"))
         self.stack.addWidget(RidePage())
 
@@ -294,9 +440,23 @@ class MainWindow(QMainWindow):
         h.addWidget(left)
         h.addWidget(self.stack, 1)
 
+        self.login_tab.logged_in.connect(self.after_login)
+
     def goto_app(self):
         """Switch to the main app after clicking 'Continue'."""
         self.root.setCurrentIndex(1)
+
+    def after_login(self, user_preview):
+        """called on emitted signal"""
+        name = user_preview.get("name", "")
+        email = user_preview.get("email", "")
+        username = user_preview.get("username", "")
+        area = user_preview.get("area", "")
+        self.profile_label.setText(f"Welcome, {name}!")
+        self.root.setCurrentIndex(1)
+        self.btn_profile.setChecked(True)
+        self.stack.setCurrentIndex(0)
+        #self.profile_label.setText(info)
 
 
 def main():
